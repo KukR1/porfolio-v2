0df06968255acb0f54749f13381294cb8ed00d8a
KukR1 <nikdani123@hotmail.com>
2022-08-19 17:52:56 +0300

    URL changes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -17,18 +17,18 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Nikita's homepage" />
         <meta name="author" content="Nikitas Danilov" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="nik-dani" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Nikitas Danilov" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:site" content="@nik-dani" />
+        <meta name="twitter:creator" content="@nik-dani" />
+        <meta name="twitter:image" content="https://ibb.co/yBn6gk1" />
         <meta property="og:site_name" content="Nikitas Danilov" />
         <meta name="og:title" content="Nikitas Danilov" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="https://ibb.co/yBn6gk1" />
         <title>Nikitas Danilov - Homepage</title>
       </Head>
 
